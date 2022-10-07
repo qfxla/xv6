d488f44e15b84e14f0a4dd72dd12321c50bc3dfe
qfxla <2690357721@qq.com>
2022-10-07 20:20:20 +0800

    lazy init

---
+++ cmake-build-debug/CMakeFiles/xv6.dir/DependInfo.cmake
@@ -0,0 +1,70 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/bio.c" "CMakeFiles/xv6.dir/kernel/bio.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/bio.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/console.c" "CMakeFiles/xv6.dir/kernel/console.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/console.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/exec.c" "CMakeFiles/xv6.dir/kernel/exec.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/exec.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/file.c" "CMakeFiles/xv6.dir/kernel/file.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/file.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/fs.c" "CMakeFiles/xv6.dir/kernel/fs.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/fs.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/kalloc.c" "CMakeFiles/xv6.dir/kernel/kalloc.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/kalloc.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/log.c" "CMakeFiles/xv6.dir/kernel/log.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/log.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/main.c" "CMakeFiles/xv6.dir/kernel/main.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/main.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/pipe.c" "CMakeFiles/xv6.dir/kernel/pipe.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/pipe.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/plic.c" "CMakeFiles/xv6.dir/kernel/plic.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/plic.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/printf.c" "CMakeFiles/xv6.dir/kernel/printf.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/printf.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/proc.c" "CMakeFiles/xv6.dir/kernel/proc.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/proc.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/ramdisk.c" "CMakeFiles/xv6.dir/kernel/ramdisk.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/ramdisk.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/sleeplock.c" "CMakeFiles/xv6.dir/kernel/sleeplock.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/sleeplock.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/spinlock.c" "CMakeFiles/xv6.dir/kernel/spinlock.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/spinlock.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/start.c" "CMakeFiles/xv6.dir/kernel/start.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/start.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/string.c" "CMakeFiles/xv6.dir/kernel/string.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/string.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/syscall.c" "CMakeFiles/xv6.dir/kernel/syscall.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/syscall.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/sysfile.c" "CMakeFiles/xv6.dir/kernel/sysfile.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/sysfile.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/sysproc.c" "CMakeFiles/xv6.dir/kernel/sysproc.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/sysproc.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/trap.c" "CMakeFiles/xv6.dir/kernel/trap.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/trap.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/uart.c" "CMakeFiles/xv6.dir/kernel/uart.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/uart.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/virtio_disk.c" "CMakeFiles/xv6.dir/kernel/virtio_disk.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/virtio_disk.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/kernel/vm.c" "CMakeFiles/xv6.dir/kernel/vm.c.obj" "gcc" "CMakeFiles/xv6.dir/kernel/vm.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/mkfs/mkfs.c" "CMakeFiles/xv6.dir/mkfs/mkfs.c.obj" "gcc" "CMakeFiles/xv6.dir/mkfs/mkfs.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/cat.c" "CMakeFiles/xv6.dir/user/cat.c.obj" "gcc" "CMakeFiles/xv6.dir/user/cat.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/echo.c" "CMakeFiles/xv6.dir/user/echo.c.obj" "gcc" "CMakeFiles/xv6.dir/user/echo.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/find.c" "CMakeFiles/xv6.dir/user/find.c.obj" "gcc" "CMakeFiles/xv6.dir/user/find.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/forktest.c" "CMakeFiles/xv6.dir/user/forktest.c.obj" "gcc" "CMakeFiles/xv6.dir/user/forktest.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/grep.c" "CMakeFiles/xv6.dir/user/grep.c.obj" "gcc" "CMakeFiles/xv6.dir/user/grep.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/grind.c" "CMakeFiles/xv6.dir/user/grind.c.obj" "gcc" "CMakeFiles/xv6.dir/user/grind.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/init.c" "CMakeFiles/xv6.dir/user/init.c.obj" "gcc" "CMakeFiles/xv6.dir/user/init.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/kill.c" "CMakeFiles/xv6.dir/user/kill.c.obj" "gcc" "CMakeFiles/xv6.dir/user/kill.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/lazytests.c" "CMakeFiles/xv6.dir/user/lazytests.c.obj" "gcc" "CMakeFiles/xv6.dir/user/lazytests.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/ln.c" "CMakeFiles/xv6.dir/user/ln.c.obj" "gcc" "CMakeFiles/xv6.dir/user/ln.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/ls.c" "CMakeFiles/xv6.dir/user/ls.c.obj" "gcc" "CMakeFiles/xv6.dir/user/ls.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/mkdir.c" "CMakeFiles/xv6.dir/user/mkdir.c.obj" "gcc" "CMakeFiles/xv6.dir/user/mkdir.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/pingpong.c" "CMakeFiles/xv6.dir/user/pingpong.c.obj" "gcc" "CMakeFiles/xv6.dir/user/pingpong.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/primes.c" "CMakeFiles/xv6.dir/user/primes.c.obj" "gcc" "CMakeFiles/xv6.dir/user/primes.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/printf.c" "CMakeFiles/xv6.dir/user/printf.c.obj" "gcc" "CMakeFiles/xv6.dir/user/printf.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/rm.c" "CMakeFiles/xv6.dir/user/rm.c.obj" "gcc" "CMakeFiles/xv6.dir/user/rm.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/sh.c" "CMakeFiles/xv6.dir/user/sh.c.obj" "gcc" "CMakeFiles/xv6.dir/user/sh.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/sleep.c" "CMakeFiles/xv6.dir/user/sleep.c.obj" "gcc" "CMakeFiles/xv6.dir/user/sleep.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/stressfs.c" "CMakeFiles/xv6.dir/user/stressfs.c.obj" "gcc" "CMakeFiles/xv6.dir/user/stressfs.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/sysinfotest.c" "CMakeFiles/xv6.dir/user/sysinfotest.c.obj" "gcc" "CMakeFiles/xv6.dir/user/sysinfotest.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/trace.c" "CMakeFiles/xv6.dir/user/trace.c.obj" "gcc" "CMakeFiles/xv6.dir/user/trace.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/ulib.c" "CMakeFiles/xv6.dir/user/ulib.c.obj" "gcc" "CMakeFiles/xv6.dir/user/ulib.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/umalloc.c" "CMakeFiles/xv6.dir/user/umalloc.c.obj" "gcc" "CMakeFiles/xv6.dir/user/umalloc.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/usertests.c" "CMakeFiles/xv6.dir/user/usertests.c.obj" "gcc" "CMakeFiles/xv6.dir/user/usertests.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/wc.c" "CMakeFiles/xv6.dir/user/wc.c.obj" "gcc" "CMakeFiles/xv6.dir/user/wc.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/xargs.c" "CMakeFiles/xv6.dir/user/xargs.c.obj" "gcc" "CMakeFiles/xv6.dir/user/xargs.c.obj.d"
+  "D:/app/CLion 2022.2.3/ClionProjects/6.S081/xv6/user/zombie.c" "CMakeFiles/xv6.dir/user/zombie.c.obj" "gcc" "CMakeFiles/xv6.dir/user/zombie.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- kernel/sysinfo.h
+++
@@ -1,6 +0,0 @@
-#include "types.h"
-
-struct sysinfo {
-  uint64 freemem;   // amount of free memory (bytes)
-  uint64 nproc;     // number of process
-};
